bd96ef7ea5b9ee8ed745beadcd5ab9162c10dd02
Shaleen Jain <shaleen@jain.sh>
2021-10-19 12:14:18 +0000

    fix: changes from danil's review

--- buidler.config.js
+++ buidler.config.js
@@ -23,7 +23,7 @@ task("accounts", "Prints the list of accounts", async (_, { ethers }) => {
 module.exports = {
   // This is a sample solc configuration that specifies which version of solc to use
   solc: {
-    version: "0.7.1",
+    version: "0.7.6",
     optimizer: {
       enabled: true,
       runs: 200,
